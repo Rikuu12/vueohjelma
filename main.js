d503143f3961678d2d5fce2fb556fa4019952f08
Riku Silventoinen <riku.silventoinen1@gmail.com>
2022-10-07 08:51:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'boots',
+            image: './Kuvat/socks_green.jpg',
+            url: 'https://www.vuemastery.com/'
+        }
+    }
+})  
